bdb22e414fa0eb370045914f49caf00d43ef81e2
CarstenSchliz <schlizcarsten@gmail.com>
2025-08-20 23:19:07 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// Deine zentrale Config
+const firebaseConfig = {
+  apiKey: "AIzaSyDIuKwYoKQDyzy6qpmY2LGahJofZx6qnuw",
+  authDomain: "iuk-app.firebaseapp.com",
+  projectId: "iuk-app",
+  storageBucket: "iuk-app.firebasestorage.app",
+  messagingSenderId: "759014128178",
+  appId: "1:759014128178:web:09c3690cd95b402c8ada2b",
+  measurementId: "G-ZPD5VPD5TS"
+};
+
+// App initialisieren
+const app = initializeApp(firebaseConfig);
+
+// Auth exportieren
+export const auth = getAuth(app);
